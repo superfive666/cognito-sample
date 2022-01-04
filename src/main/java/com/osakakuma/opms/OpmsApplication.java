5c550aa71c2a22ad60165529e1743cf835df8a13
Superfive <chao.wu@aquariux.com>
2022-01-04 20:14:42 +0800

    initial commit

---
+++ src/main/java/com/osakakuma/opms/OpmsApplication.java
@@ -0,0 +1,13 @@
+package com.osakakuma.opms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpmsApplication.class, args);
+	}
+
+}
